63ad1c87ae9f80a6805ef3c3a49b3f12ee231fe7
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2015-07-17 21:09:55 -0400

    Remove Sean Connery cheek from hello.c

--- src/test/rv/hello.c
+++ src/test/rv/hello.c
@@ -3,7 +3,16 @@
 
 int main() {
 
-  printf("Sukkit Trebek!\n");
+  printf("\n               === ===\n"
+         "                \\\\ //\n"
+         "        ---      \\ /\n"
+         "T H E  | X |  F I L E S\n"
+         "        ---      / \\ \n"
+         "                // \\\\ \n"
+         "               === ===\n"
+         "\n"
+         "  GILLIAN\n"
+         "  ANDERSON as DANA\n");
 
   return 0;
 }
