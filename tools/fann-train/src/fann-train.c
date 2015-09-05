9491be947984bebb3424ea906e4f00a2fe9948ce
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2015-09-05 17:26:21 -0400

    Add bit fail limit to fann-train

--- tools/fann-train/src/fann-train.c
+++ tools/fann-train/src/fann-train.c
@@ -3,25 +3,27 @@
 #include "fann.h"
 
 int main (int argc, char * argv[]) {
-  int i, j, k, num_epochs;
+  int i, j, k, max_epochs, num_bits_failing;
   struct fann * ann;
   struct fann_train_data * data;
-  fann_type * calc_out;
+  fann_type * calc_out, bit_fail_limit;
 
-  if (argc != 4) {
+  if (argc != 5) {
     printf("[ERROR] Wrong number of input arguments\n");
-    printf("Usage: fann-train <net> <training file> <num epochs>\n");
+    printf("Usage: fann-train <net> <training file> <bit fail limit> <max epochs>\n");
     return -1;
   }
 
   ann = fann_create_from_file(argv[1]);
   data = fann_read_train_from_file(argv[2]);
-  num_epochs = atoi(argv[3]);
+  bit_fail_limit = atof(argv[3]);
+  max_epochs = atoi(argv[4]);
 
   ann->training_algorithm = FANN_TRAIN_BATCH;
 
-  for (j = 0; j < num_epochs; j++) {
+  for (j = 0; j < max_epochs; j++) {
     fann_train_epoch(ann, data);
+    num_bits_failing = 0;
     for (i = 0; i < fann_length_train_data(data); i++) {
       fann_reset_MSE(ann);
       calc_out = fann_test(ann, data->input[i], data->output[i]);
@@ -31,14 +33,18 @@ int main (int argc, char * argv[]) {
       }
       for (k = 0; k < data->num_output; k++) {
         printf("%8.5f ", calc_out[k]);
+        num_bits_failing += fabs(calc_out[k] - data->output[i][k]) > bit_fail_limit;
       }
       if (i < fann_length_train_data(data) - 1)
         printf("\n");
     }
-    printf("%8.5f\n\n", fann_get_MSE(ann));
+    printf("%5d\n\n", j);
+    if (num_bits_failing == 0)
+      goto done;
+    // printf("%8.5f\n\n", fann_get_MSE(ann));
   }
 
-
+ done:
   fann_destroy(ann);
   fann_destroy_train(data);
 
