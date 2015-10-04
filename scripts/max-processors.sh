f842b883296fa09bf53f14356ec88d94e6142527
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2015-10-04 16:09:04 -0400

    Fix max-processors bug, make more aggressive
    Fixes a bug in max-processors and makes this more aggressive in terms of
    grabbing cores---this will now look at the best-case load from
    /proc/loadavg instead of the worst-case load.

--- scripts/max-processors.sh
+++ scripts/max-processors.sh
@@ -12,13 +12,13 @@ NUM_CPUS=`cat /proc/cpuinfo | \
 # average intevals
 WORST_LOAD=`cat /proc/loadavg | \
     awk '{print $1"\n"$2"\n"$3}' | \
-    sort -nr | \
+    sort -n | \
     head -n1`
 
 # Grab the number of idle CPUs that we see, and, if the machine is
 # heavily loaded, just grab one
 COMMANDEERED_CPUS=`echo "$NUM_CPUS $WORST_LOAD-p" | dc | sed 's/\..\+$//'`
-if [[ $COMMANDEERED_CPUS -lt 0 ]]; then
+if [[ $COMMANDEERED_CPUS -lt 1 ]]; then
    COMMANDEERED_CPUS=1;
 fi
 echo $COMMANDEERED_CPUS
