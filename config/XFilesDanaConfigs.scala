cfa36e7e2d3a24c1e8adf3361657f80402939164
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2015-12-02 12:11:52 -0500

    Cleanup top level configs

--- config/XFilesDanaConfigs.scala
+++ config/XFilesDanaConfigs.scala
@@ -31,63 +31,24 @@ class XFilesDanaNoLearningConfig extends Config (
 class XFilesDanaCPPConfig extends Config(new XFilesDanaConfig ++
   new DefaultXFilesDanaFPGAConfig ++ new DefaultCPPConfig)
 
-class XFilesDanaNoLearningCPPConfig extends Config(new XFilesDanaNoLearningConfig ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++ new DefaultCPPConfig)
+class XFilesDanaNoLearningCPPConfig extends Config(
+  new XFilesDanaNoLearningConfig ++ new XFilesDanaCPPConfig)
 
-class XFilesDanaFPGAConfig extends Config(new XFilesDanaConfig ++
-  new DefaultXFilesDanaFPGAConfig ++ new DefaultFPGAConfig)
+class XFilesDanaFPGAConfig extends Config(new XFilesDanaCPPConfig)
 
-class XFilesDanaNoLearningFPGAConfig extends Config(new XFilesDanaNoLearningConfig ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++ new DefaultFPGAConfig)
+class XFilesDanaNoLearningFPGAConfig extends Config(
+  new XFilesDanaNoLearningCPPConfig)
 
 class XFilesDanaFPGASmallConfig extends Config(new XFilesDanaConfig ++
   new DefaultXFilesDanaFPGAConfig ++ new DefaultFPGASmallConfig)
 
 // Variants that use explicit numbers of PEs
-class DanaPE1 extends Config (
-  topDefinitions = { (pname,site,here) => pname match {
-      case PeTableNumEntries => 1 }})
+class DanaPEX(numPes: Int) extends Config(
+  knobValues = { case "NUM_PES" => numPes })
 
-class DanaPE2 extends Config (
-  topDefinitions = { (pname,site,here) => pname match {
-      case PeTableNumEntries => 2 }})
-
-class DanaPE3 extends Config (
-  topDefinitions = { (pname,site,here) => pname match {
-      case PeTableNumEntries => 3 }})
-
-class DanaPE4 extends Config (
-  topDefinitions = { (pname,site,here) => pname match {
-      case PeTableNumEntries => 4 }})
-
-class DanaPE5 extends Config (
-  topDefinitions = { (pname,site,here) => pname match {
-      case PeTableNumEntries => 5 }})
-
-class DanaPE6 extends Config (
-  topDefinitions = { (pname,site,here) => pname match {
-      case PeTableNumEntries => 6 }})
-
-class XFilesDanaPE1Config extends Config(new DanaPE1 ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++
-  new DefaultFPGAConfig)
-
-class XFilesDanaPE2Config extends Config(new DanaPE2 ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++
-  new DefaultFPGAConfig)
-
-class XFilesDanaPE3Config extends Config(new DanaPE3 ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++
-  new DefaultFPGAConfig)
-
-class XFilesDanaPE4Config extends Config(new DanaPE4 ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++
-  new DefaultFPGAConfig)
-
-class XFilesDanaPE5Config extends Config(new DanaPE5 ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++
-  new DefaultFPGAConfig)
-
-class XFilesDanaPE6Config extends Config(new DanaPE6 ++
-  new XFilesDanaConfig ++ new DefaultXFilesDanaFPGAConfig ++
-  new DefaultFPGAConfig)
+class XFilesDanaPE1Config extends Config(new DanaPEX(1) ++ new XFilesDanaCPPConfig)
+class XFilesDanaPE2Config extends Config(new DanaPEX(2) ++ new XFilesDanaCPPConfig)
+class XFilesDanaPE3Config extends Config(new DanaPEX(3) ++ new XFilesDanaCPPConfig)
+class XFilesDanaPE4Config extends Config(new DanaPEX(4) ++ new XFilesDanaCPPConfig)
+class XFilesDanaPE5Config extends Config(new DanaPEX(5) ++ new XFilesDanaCPPConfig)
+class XFilesDanaPE6Config extends Config(new DanaPEX(6) ++ new XFilesDanaCPPConfig)
