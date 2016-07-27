762ba73e71e1bca9d54d8b0f7e5ddb508b603d0a
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2016-07-27 19:32:53 -0400

    Re-sync with rocket master (change to status bits)
    This aligns with the new RoCCInterface that puts the status bits inside
    the cmd.

--- src/main/scala/Dana.scala
+++ src/main/scala/Dana.scala
@@ -277,7 +277,7 @@ class Dana(implicit p: Parameters) extends XFilesBackend()(p)
   antw.io.xfiles.rocc.cmd.valid := io.rocc.cmd.valid
   antw.io.xfiles.rocc.cmd.bits := io.rocc.cmd.bits
   antw.io.xfiles.rocc.resp.ready := io.rocc.resp.ready
-  antw.io.xfiles.rocc.status := io.rocc.status
+  antw.io.xfiles.rocc.status := io.rocc.cmd.bits.status
 
   antw.io.cache <> cache.io.mem
   antw.io.xfiles.dcache.mem <> io.rocc.mem
@@ -300,7 +300,7 @@ class Dana(implicit p: Parameters) extends XFilesBackend()(p)
   tTable.io.arbiter.rocc.cmd.valid := io.rocc.cmd.valid
   tTable.io.arbiter.rocc.cmd.bits := io.rocc.cmd.bits
   tTable.io.arbiter.rocc.resp.ready := io.rocc.resp.ready
-  tTable.io.arbiter.rocc.status := io.rocc.status
+  tTable.io.arbiter.rocc.status := io.rocc.cmd.bits.status
 
   tTable.io.control <> control.io.tTable
   tTable.io.regFile <> regFile.io.tTable
