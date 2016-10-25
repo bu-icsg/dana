228338c85478599b139bae7fc848fb0d7019e9c3
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2016-10-25 18:21:37 -0400

    Use a BasicTester for Standalone operation

--- src/main/scala/standalone/Standalone.scala
+++ src/main/scala/standalone/Standalone.scala
@@ -1,11 +1,12 @@
 package rocketchip
 
 import chisel3._
+import chisel3.testers.BasicTester
 import cde.{Field, Parameters}
 import rocket._
 import xfiles._
 import dana._
 
-class Standalone(implicit p: Parameters) extends RoCC {
+class Standalone(implicit p: Parameters) extends BasicTester {
   val xfiles = Module(new XFiles)
 }
