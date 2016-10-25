76f5c68931406d081169678877effc8ed7d7e4d7
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2016-10-25 18:07:00 -0400

    Standalone XFiles instance needs a Module wrapper

--- src/main/scala/standalone/Standalone.scala
+++ src/main/scala/standalone/Standalone.scala
@@ -1,10 +1,11 @@
 package rocketchip
 
+import chisel3._
 import cde.{Field, Parameters}
 import rocket._
 import xfiles._
 import dana._
 
 class Standalone(implicit p: Parameters) extends RoCC {
-  val xfiles = new XFiles
+  val xfiles = Module(new XFiles)
 }
