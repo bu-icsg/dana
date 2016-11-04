10cdf9d5790607bcd00304be44c87e39173cef55
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2016-11-04 15:22:08 -0400

    Cleanup standalone

--- src/main/scala/standalone/DebugTests.scala
+++ src/main/scala/standalone/DebugTests.scala
@@ -8,31 +8,29 @@ import xfiles.XFilesUserRequests
 class DebugTester(implicit p: Parameters) extends XFilesTester {
   val s_INIT :: s_WRITE :: s_READ :: s_DONE :: Nil = Enum(UInt(), 4)
 
-  val t_ECHO :: t_L1R :: Nil = Enum(UInt(), 2)
+  val t_ECHO :: t_L1R :: t_L1W :: Nil = Enum(UInt(), 3)
 
-  val state = Reg(UInt(width = log2Up(4)), init = s_INIT)
+  val state = Reg(UInt(width = log2Up(16)), init = s_INIT)
 
   val test = Reg(UInt(width = 32), init = UInt(0))
 
   when (state === s_INIT) { state := s_WRITE }
 
+  val data = Seq(0xaaaa, 0xbbbb, 0xcccc, 0xdddd)
   when (state === s_WRITE) {
     switch (test) {
-      is (UInt(0)) { debug_echo_via_reg(0xdead) }
-      is (UInt(1)) { debug_read_mem(0) }
+      is (t_ECHO) { debug_echo_via_reg(data(0)) }
     }
     state := s_READ
   }
 
   when (state === s_READ && dut.io.resp.fire()) {
     printf("[INFO] Saw response 0x%x\n", dut.io.resp.bits.data)
-    state := Mux(test === UInt(1), s_DONE, s_READ)
+    state := Mux(test === t_ECHO, s_DONE, s_READ)
     test := test + UInt(1)
     switch (test) {
-      is (UInt(0)) { assert (dut.io.resp.bits.data === UInt("hdead"),
+      is (t_ECHO) { assert (dut.io.resp.bits.data === UInt(data(0)),
         "XFiles did not echo sent data") }
-      is (UInt(1)) { assert (dut.io.resp.bits.data === UInt("hbeef"),
-        "XFiles did not read correct L1 data") }
     }
   }
 
--- src/main/scala/standalone/Standalone.scala
+++ src/main/scala/standalone/Standalone.scala
@@ -19,31 +19,33 @@ class HoneyPot[T <: Bundle](name: String = "") extends Module {
   assert(!(io.req.valid), s"Module tried to access HoneyPot $name")
 }
 
-abstract class RoccTester[T <: RoCC](x: Int = 0)(implicit p: Parameters)
+abstract class RoccTester[T <: RoCC](implicit p: Parameters)
     extends BasicTester {
   def dut: T
+  def xCustomType: Int
 
-  val status = Reg(new MStatus)
+  private val status = Reg(new MStatus)
   status.elements map { case (s: String, d: Data) => d := UInt(0) }
 
   def xcustom(funct: Int, rd: Int = 0, rs1: Int = 0, rs2: Int = 0,
-    rs1_d: Long = 0, rs2_d: Long = 0) {
-    dut.io.cmd.valid := Bool(true)
-    dut.io.cmd.bits.inst.rd := UInt(rd)
-    dut.io.cmd.bits.inst.xd := UInt(rd) =/= UInt(0)
-    dut.io.cmd.bits.inst.rs1 := UInt(rs1)
-    dut.io.cmd.bits.inst.rs2 := UInt(rs2)
-    dut.io.cmd.bits.inst.funct := UInt(funct)
-    dut.io.cmd.bits.inst.opcode := { x match {
+    rs1_d: Long = 0, rs2_d: Long = 0): RoCCCommand = {
+    val tmp = Wire(new RoCCCommand)
+    tmp.inst.rd := UInt(rd)
+    tmp.inst.xd := UInt(rd) =/= UInt(0)
+    tmp.inst.rs1 := UInt(rs1)
+    tmp.inst.rs2 := UInt(rs2)
+    tmp.inst.funct := UInt(funct)
+    tmp.inst.opcode := { xCustomType match {
       case 0 => UInt("b0001011")
       case 1 => UInt("b0101011")
       case 2 => UInt("b1011011")
       case 3 => UInt("b1111011")
       case _ => throw new Exception("Undefined XCustom type (should be [0,3])")
     }}
-    dut.io.cmd.bits.rs1 := UInt(rs1_d)
-    dut.io.cmd.bits.rs2 := UInt(rs2_d)
-    dut.io.cmd.bits.status := status
+    tmp.rs1 := UInt(rs1_d)
+    tmp.rs2 := UInt(rs2_d)
+    tmp.status := status
+    tmp
   }
 
   def setPrv(prv: Int) {
@@ -56,6 +58,7 @@ abstract class RoccTester[T <: RoCC](x: Int = 0)(implicit p: Parameters)
 class XFilesTester(implicit p: Parameters) extends RoccTester[XFiles]
     with XFilesUserRequests with XFilesDebugActions {
   val dut = Module(new XFiles)
+  val xCustomType = 0
 
   // Memory Honeypot
   val mem = Module(new HoneyPot(name="Memory"))
@@ -67,11 +70,12 @@ class XFilesTester(implicit p: Parameters) extends RoccTester[XFiles]
 
   private def _debug_test(action: Int, data: Int = 0, rd: Int = 1, addr: Int = 0) {
     val action_and_data: Long = (action.asInstanceOf[Long] << 32) | (data & ~(~0L << 32))
-    xcustom(funct=t_USR_XFILES_DEBUG, rd=rd, rs1_d=action_and_data, rs2_d=addr)
+    dut.io.cmd.valid := Bool(true)
+    dut.io.cmd.bits := xcustom(t_USR_XFILES_DEBUG, rd, rs1_d=action_and_data, rs2_d=addr)
   }
 
-  def debug_echo_via_reg(data: Int) { _debug_test(a_REG, data) }
-
-  def debug_read_mem(addr: Int) { _debug_test(a_MEM_READ, addr=addr) }
+  def debug_echo_via_reg(d: Int) { _debug_test(a_REG, data=d) }
+  def debug_read_mem(a: Int) { _debug_test(a_MEM_READ, addr=a) }
+  def debug_write_mem(a: Int, d: Int) { _debug_test(a_MEM_WRITE, addr=a, data=d) }
 
 }
