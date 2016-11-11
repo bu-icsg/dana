406e015bc75e56f78d47aaf621642314c21c2dd0
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2016-11-10 21:09:57 -0500

    WIP commit on generate-ant

--- tools/src/generate-ant.c
+++ tools/src/generate-ant.c
@@ -8,7 +8,7 @@
 #include "src/main/c/xfiles-asid-nnid-table.h"
 
 void usage() {
-  printf("Usage: generate-ant [OPTIONS]"
+  printf("Usage: generate-ant -a [asid],[nn-config] [OPTIONS]"
          "\n"
          "Options:\n"
          ""
@@ -50,6 +50,26 @@ int parse_asid_file(char * t, t_asid_file * af) {
   return 0;
 }
 
+int ant_dump(ant * table, FILE * file, int verbose) {
+  void * offset = table->entry_v;
+  void * last = offset;
+  if (verbose) printf("[INFO] Offset: %p\n", offset);
+  for (ant_entry * e = table->entry_v; e < &table->entry_v[table->size]; e++) {
+    fwrite(e, sizeof(ant_entry), 1, file);
+    last += sizeof(ant_entry);
+    for (nn_config * n = e->asid_nnid_v; n < &e->asid_nnid_v[e->num_valid]; n++) {
+      long int padding = (long int) n - (long int) last;
+      char zero = 0;
+      for (int i = 0; i < padding; i++)
+        fwrite(&zero, 1, 1, file);
+      fwrite(n, sizeof(nn_config), 1, file);
+      last += padding + sizeof(nn_config);
+      // [TODO] Write the contents of the NN configuration
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char ** argv) {
   int exit_code = 0;
 
@@ -67,14 +87,16 @@ int main(int argc, char ** argv) {
 
   // Parse command line options
   int c;
+  static int opt_verbose;
   while (1) {
     static struct option long_test[] = {
-      {"attach",               required_argument, 0, 'a'},
-      {"help",                 no_argument,       0, 'h'},
+      {"attach",               required_argument, 0,           'a'},
+      {"help",                 no_argument,       0,           'h'},
+      {"verbose",              no_argument,       &opt_verbose, 1},
       {0, 0, 0, 0}
     };
     int option_index = 0;
-    c = getopt_long(argc, argv, "a:h", long_test, &option_index);
+    c = getopt_long(argc, argv, "a:hv", long_test, &option_index);
     if (c == -1)
       break;
     switch (c) {
@@ -85,7 +107,7 @@ int main(int argc, char ** argv) {
           s_af.n = s_af.n * 2;
         }
         if (parse_asid_file(optarg, &s_af.af[s_af.i++])) {
-          printf("[ERROR] Bad asid,file pair: %s\n", optarg);
+          fprintf(stderr, "[ERROR] Bad asid,file pair: %s\n", optarg);
           exit_code = 1;
           goto bail;
         }
@@ -96,18 +118,41 @@ int main(int argc, char ** argv) {
       case 'h': usage(); return 0;
     }
   };
-
-  for (int i = 0; i < s_af.i; i++) {
-    printf("[INFO] asid: %d\n", s_af.af[i].asid);
-    printf("[INFO] file: %s\n", s_af.af[i].file);
+  // Ensure that at least one asid,network tuple was given.
+  if (s_af.i == 0) {
+    fprintf(stderr, "[ERROR] Specify at least one `asid,network` tuple\n");
+    usage();
+    exit_code = 1;
+    goto bail;
   }
 
-  if (s_af.i > 0) printf("[INFO] max asid: %d\n", s_af.max_asid);
-
+  // Create and populate the ASID-NNID Table
   ant * table;
-  asid_nnid_table_create(&table, s_af.max_asid, 10); // [TODO] Magic number
+  asid_nnid_table_create(&table, s_af.max_asid + 1, 10); // [TODO] Magic number
+  for (t_asid_file * x = s_af.af; x < s_af.af + s_af.i; x++) {
+    if (attach_nn_configuration(&table, x->asid, x->file) == -1) {
+      fprintf(stderr, "[ERROR] Failed to attached to ASID %d file %s\n",
+              x->asid, x->file);
+      exit_code = 2;
+      goto bail;
+    }
+    if (opt_verbose) printf("[INFO] asid: %d\n"
+                            "[INFO] file: %s\n", x->asid, x->file);
+  }
+  if (opt_verbose) printf("[INFO] max asid: %d\n", s_af.max_asid);
+  if (opt_verbose) asid_nnid_table_info(table);
+
+  // Dump the raw bits
+  FILE * file = NULL;
+  if ((file = fopen("dump.bin", "w")) == 0) {
+    fprintf(stderr, "[ERROR] Unable to open dump file\n");
+    exit_code = 3;
+    goto bail;
+  }
+  ant_dump(table, file, opt_verbose);
 
 bail:
-  free(s_af.af);
+  if (s_af.af != NULL) free(s_af.af);
+  if (file != NULL) fclose(file);
   return exit_code;
 }
