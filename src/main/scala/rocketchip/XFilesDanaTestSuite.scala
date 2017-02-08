31e24cfb7d193698f88ca9764d1f6a76db50d4ff
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-02-08 11:07:12 -0500

    Updates to Assembly/Regression tests names

--- src/main/scala/rocketchip/XFilesDanaTestSuite.scala
+++ src/main/scala/rocketchip/XFilesDanaTestSuite.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.LinkedHashSet
 
 class AssemblyTests(rocc: String, testDir: String,
   names: LinkedHashSet[String])(envName: String) extends
-    rocketchip.AssemblyTestSuite(rocc, names)(envName) {
+    rocketchip.AssemblyTestSuite(rocc + "-" + testDir, names)(envName) {
   override val dir = s"$$(base_dir)/$rocc/tests/build/$testDir"
 }
 
@@ -27,13 +27,13 @@ object XFilesDanaTestSuites {
   )
 
   val xfilesDanaRegrTestNames = LinkedHashSet (
-    "xfiles-dana-p-debug",
-    "xfiles-dana-p-id",
-    "xfiles-dana-p-xorSigmoidSymmetric"
+    "xfiles-dana-smoke-p-debug",
+    "xfiles-dana-smoke-p-id",
+    "xfiles-dana-nets-p-xorSigmoidSymmetric"
   )
 
-  val xfilesDanaSmoke = new AssemblyTests("xfiles-dana", "all_tests", smoke)(_)
-  val xfilesDanaNets = new AssemblyTests("xfiles-dana", "all_tests", nets)(_)
+  val xfilesDanaSmoke = new AssemblyTests("xfiles-dana", "smoke", smoke)(_)
+  val xfilesDanaNets = new AssemblyTests("xfiles-dana", "nets", nets)(_)
   val xfilesDanaRegressions = new RegressionTests("xfiles-dana", "all_tests",
     xfilesDanaRegrTestNames)
 }
