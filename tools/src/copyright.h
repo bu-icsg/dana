18a9353fa73478768c429967291e072596165b58
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-03-14 00:13:01 -0400

    Print FANN copyright notice to stderr

--- tools/src/copyright.h
+++ tools/src/copyright.h
@@ -7,6 +7,6 @@
 
 #define COPYRIGHT_FANN "Portions Copyright (C) 2003-2016 Steffen Nissen\n"
 
-#define PRINT_NOTICES(text) printf(text "\n")
+#define PRINT_NOTICES(text) fprintf(stderr, text "\n")
 
 #endif  // __TOOLS_SRC_COPYRIGHT_H_
