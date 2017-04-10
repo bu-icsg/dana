1859ea541fed53c4967b72b9fdceb9f6846705cf
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-04-10 00:12:46 -0400

    Fix trap test set_asid signature
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/test/rv/trap-00-supervisor-req-as-user.c
+++ src/test/rv/trap-00-supervisor-req-as-user.c
@@ -3,7 +3,9 @@
 #include "src/include/xfiles-user-pk.h"
 
 int main() {
+  asid_type asid = 2;
+  tid_type tid = 0;
   pk_syscall_set_asid(1);
-  set_asid(2);
+  set_asid(&asid, &tid);
   while(1) {};
 }
